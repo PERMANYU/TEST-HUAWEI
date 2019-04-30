dcd15875da94e02d7a3345a82537e52f38389771
LULULU <LuoPu1113@163.com>
2019-04-30 13:57:59 +0800

    代码提交

--- CMB/config/proxy.js
+++ CMB/config/proxy.js
@@ -1,16 +1,30 @@
 module.exports = {
-	'/cmb/cmbdatainterfaceconvert':{
-		target:'http://10.145.131.187:8169',
-		changeOrigin: true,
-		pathRewrite:{
-			'': '/'
-		}
-	},
-    '/cpc':{
-        target:'http://10.145.131.201:8181',
+    '/dev/proxy/api':{
+        target:'http://10.145.209.146:8065',
         changeOrigin: true,
         pathRewrite:{
-            '^/cpc': '/'
+            '^/dev/proxy/api': '/'
+        }
+    },
+    '/release/proxy/api':{
+        target:'http://10.145.131.187:8169',
+        changeOrigin: true,
+        pathRewrite:{
+            '^/release/proxy/api': '/'
+        }
+    },
+    '/ue/proxy/api':{
+        target:'http://fe.ue.sh.ctc.com',
+        changeOrigin: true,
+        pathRewrite:{
+            '^/ue/proxy/api': '/'
+        }
+    },
+    '/pr/proxy/api':{
+        target:'http://fe.pr.sh.ctc.com',
+        changeOrigin: true,
+        pathRewrite:{
+            '^/pr/proxy/api': '/'
         }
     }
 }
